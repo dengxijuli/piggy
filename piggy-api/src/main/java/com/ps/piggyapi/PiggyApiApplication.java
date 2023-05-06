46c8b3e28f2ef4e4b55edf5d29f679e74a445507
dx <2936322204@qq.com>
2023-05-06 20:19:42 +0800

    一次测试

---
+++ piggy-api/src/main/java/com/ps/piggyapi/PiggyApiApplication.java
@@ -0,0 +1,13 @@
+package com.ps.piggyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiggyApiApplication.class, args);
+    }
+
+}
